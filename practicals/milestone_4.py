9810ccd4e12ba27bda5ca8d5de07f9500ca49d71
oobwoo <oriol5@icloud.com>
2023-11-15 19:16:18 +0000

    milestone 4 initial committed

---
+++ practicals/milestone_4.py
@@ -0,0 +1,11 @@
+import random
+
+
+class Hangman:
+    def __innit__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+    
+print(word_list)
+
+        
